2f8b20347d7c54425f5c27a4e42365a1554a3b36
Brian Pruitt <brianrpruitt63@gmail.com>
2023-07-29 16:45:59 -0400

    added possession display

--- TeamSide.js
+++ TeamSide.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import {Text, View, Button} from 'react-native';
-import { ScoreContext } from './context';
+import { ScoreContext, GameDataContext } from './context';
 
 function TeamSide({scoreIntervals, team}) {
 
     const {score, incrementScore, setScore} = useContext(ScoreContext);
+    const {gameData} = useContext(GameDataContext);
 
     return(
         <View>
@@ -14,6 +15,8 @@ function TeamSide({scoreIntervals, team}) {
                 <Button key={interval}
                         title={`+${interval}`}
                         onPress={() => incrementScore(interval, team.position)} />)}
+            {gameData.possession === team.position &&
+                <Text>Possession</Text>}
         </View>
     );
 };
