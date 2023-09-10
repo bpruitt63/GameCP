7ad1c6811778c84205097b704779c0a1c81088d2
Brian Pruitt <brianrpruitt63@gmail.com>
2023-09-10 18:46:25 -0400

    set focus on manual timer form

--- ManualTimerForm.js
+++ ManualTimerForm.js
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { TextInput, Button, Text } from 'react-native';
 
 function ManualTimerForm({initialValue, save, cancel}) {
 
     const [value, setValue] = useState(initialValue);
-
+    const ref1 = useRef();
+    const ref2 = useRef();
 
     const handleMinuteChange = (val) => {
         const newValue = {...value, minutes: val};
         setValue(newValue);
+        if (newValue.minutes.length >= 2) ref2.current.focus();
     };
 
     const handleSecondChange = (val) => {
@@ -21,12 +23,17 @@ function ManualTimerForm({initialValue, save, cancel}) {
             <TextInput inputMode='numeric'
                 maxLength={2}
                 onChangeText={handleMinuteChange}
-                value={value.minutes} />
+                value={value.minutes}
+                autoFocus
+                selectTextOnFocus
+                ref={ref1} />
             <Text>:</Text>
             <TextInput inputMode='numeric'
                 maxLength={2}
                 onChangeText={handleSecondChange}
-                value={value.seconds} />
+                value={value.seconds}
+                selectTextOnFocus
+                ref={ref2} />
             <Button title='Confirm'
                     onPress={() => save(value)} />
             <Button title='Cancel'
