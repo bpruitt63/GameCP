aba3ba2e3ed0f3bb0b8291590f7f0ee538c08372
Brian Pruitt <brianrpruitt63@gmail.com>
2023-09-17 18:46:45 -0400

    added number of periods into settings

--- Settings.js
+++ Settings.js
@@ -8,7 +8,9 @@ import ManualInputForm from './ManualInputForm';
 function Settings() {
 
     const initialOpen = {footballTimer: false, basketballTimer: false, 
-                            soccerTimer: false, baseballLength: false};
+                            soccerTimer: false, footballLength: false,
+                            soccerLength: false, baseballLength: false,
+                            baseballLength: false};
     const [openForm, setOpenForm] = useState(initialOpen);
     const [compiledDefaults, setCompiledDefaults] = useState(defaultData);
     const [getStoredDefaults, defaultValues, setDefaultValues, updateDefaults] = useSettings();
@@ -31,18 +33,43 @@ function Settings() {
 
     const toggleOpen = (form) => setOpenForm({...initialOpen, [form]: true});
 
-    const save = async (newData) => {
+    // const save = async (newData) => {
+    //     const newDefaultValues = {...compiledDefaults};
+    //     const {sport} = newData;
+    //     if (sport) {
+    //         newDefaultValues[sport].minutes = +newData.minutes;
+    //         newDefaultValues[sport].seconds = +newData.seconds < 60 ? +newData.seconds : 59;
+    //     } else {
+    //         newDefaultValues.baseball.length = +newData;
+    //     };
+    //     await updateDefaults(newDefaultValues);
+    //     setOpenForm(initialOpen);
+    // };
+
+    const saveTime = async (newData) => {
         const newDefaultValues = {...compiledDefaults};
         const {sport} = newData;
-        if (sport) {
-            newDefaultValues[sport].minutes = +newData.minutes;
-            newDefaultValues[sport].seconds = +newData.seconds < 60 ? +newData.seconds : 59;
-        } else {
+        newDefaultValues[sport].minutes = +newData.minutes;
+        newDefaultValues[sport].seconds = +newData.seconds < 60 ? +newData.seconds : 59;
+        await updateDefaults(newDefaultValues);
+        setOpenForm(initialOpen);
+    };
+
+    const saveLength = async (newData, sport) => {
+        const newDefaultValues = {...compiledDefaults};
+        if (sport === 'baseball') {
             newDefaultValues.baseball.length = +newData;
+        } else {
+            newDefaultValues[sport].maxPeriod = +newData;
         };
         await updateDefaults(newDefaultValues);
         setOpenForm(initialOpen);
     };
+
+    const saveBaseballLength = (newData) => saveLength(newData, 'baseball');
+    const saveBasketballLength = (newData) => saveLength(newData, 'basketball');
+    const saveFootballLength = (newData) => saveLength(newData, 'football');
+    const saveSoccerLength = (newData) => saveLength(newData, 'soccer');
 
     const cancel = () => setOpenForm(initialOpen);
 
@@ -52,32 +79,53 @@ function Settings() {
                 <ManualTimerForm initialValue={{minutes: compiledDefaults.basketball.minutes,
                                                 seconds: compiledDefaults.basketball.seconds > 9 ? compiledDefaults.basketball.seconds : `0${compiledDefaults.basketball.seconds}`,
                                                 sport: 'basketball'}}
-                                                save={save}
+                                                save={saveTime}
                                                 cancel={cancel} />
             :
                 <Button title='Default Basketball Quarter Length'
                         onPress={() => toggleOpen('basketballTimer')} />}
+            {openForm.basketballLength ?
+                <ManualInputForm initialValue={compiledDefaults.basketball.maxPeriod}
+                                save={saveBasketballLength}
+                                cancel={cancel} />
+            :
+                <Button title='Default Basketball Periods'
+                        onPress={() => toggleOpen('basketballLength')} />}
             {openForm.footballTimer ?
                 <ManualTimerForm initialValue={{minutes: compiledDefaults.football.minutes,
                                                 seconds: compiledDefaults.football.seconds > 9 ? compiledDefaults.football.seconds : `0${compiledDefaults.football.seconds}`,
                                                 sport: 'football'}}
-                                                save={save}
+                                                save={saveTime}
                                                 cancel={cancel} />
             :
                 <Button title='Default Football Quarter Length'
                         onPress={() => toggleOpen('footballTimer')} />}
+            {openForm.footballLength ?
+                <ManualInputForm initialValue={compiledDefaults.football.maxPeriod}
+                                save={saveFootballLength}
+                                cancel={cancel} />
+            :
+                <Button title='Default Football Periods'
+                        onPress={() => toggleOpen('footballLength')} />}
             {openForm.soccerTimer ?
                 <ManualTimerForm initialValue={{minutes: compiledDefaults.soccer.minutes,
                                                 seconds: compiledDefaults.soccer.seconds > 9 ? compiledDefaults.soccer.seconds : `0${compiledDefaults.soccer.seconds}`,
                                                 sport: 'soccer'}}
-                                                save={save}
+                                                save={saveTime}
                                                 cancel={cancel} />
             :
                 <Button title='Default Soccer Period Length'
                         onPress={() => toggleOpen('soccerTimer')} />}
+            {openForm.soccerLength ?
+                <ManualInputForm initialValue={compiledDefaults.soccer.maxPeriod}
+                                save={saveSoccerLength}
+                                cancel={cancel} />
+            :
+                <Button title='Default Soccer Periods'
+                        onPress={() => toggleOpen('soccerLength')} />}
             {openForm.baseballLength ?
                 <ManualInputForm initialValue={compiledDefaults.baseball.length}
-                                    save={save}
+                                    save={saveBaseballLength}
                                     cancel={cancel} />
             :
                 <Button title='Default Innings'
