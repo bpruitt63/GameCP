99f8ad84f3c16419db166965497cfc61f241f946
Brian Pruitt <brianrpruitt63@gmail.com>
2023-12-16 18:23:53 -0500

    debug

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -34,7 +34,7 @@ const isLocalhost = Boolean(
   
       window.addEventListener("load", () => {
         const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-        console.log(swUrl)
+        
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
           checkValidServiceWorker(swUrl, config);
@@ -66,6 +66,7 @@ const isLocalhost = Boolean(
           }
           installingWorker.onstatechange = () => {
             if (installingWorker.state === "installed") {
+              console.log(navigator.serviceWorker)
               if (navigator.serviceWorker.controller) {
                 // At this point, the updated precached content has been fetched,
                 // but the previous service worker will still serve the older
