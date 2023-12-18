538ff2216c5da867a241c10c78dc948ae390dc5b
Brian Pruitt <brianrpruitt63@gmail.com>
2023-12-18 14:12:19 -0500

    added online check to score submit button

--- GameScreen/SubmitScores.js
+++ GameScreen/SubmitScores.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import { useOnline } from '../helpersAndData/hooks';
+import { menuStyles } from '../styles/menuStyles';
 
 function SubmitScores({submitScores, buttonStyle, textStyle}) {
 
+    const [online, watchOnlineStatus] = useOnline(navigator.onLine);
+
+    useEffect(() => {
+        watchOnlineStatus();
+    }, [watchOnlineStatus]);
+
     return (
         <TouchableOpacity onPress={submitScores}
-                        style={buttonStyle}>
-            <Text style={textStyle}>Save to Sporty</Text>    
+                        style={[buttonStyle, !online ? menuStyles.disabled : '']}
+                        disabled={!online}>
+            <Text style={textStyle}>Save to Sporty</Text>
+            {!online && <Text style={textStyle}>Online Connection Required</Text>}    
         </TouchableOpacity>
     );
 };
