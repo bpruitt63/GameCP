ba9355233845961ed451f39a02b8159d7cdaebd0
Brian Pruitt <brianrpruitt63@gmail.com>
2023-12-29 18:55:39 -0500

    refactored useEffect

--- GameScreen/RunningClock.js
+++ GameScreen/RunningClock.js
@@ -22,18 +22,50 @@ function RunningClock({currentTime, setCurrentTime, stopTimer, saveTime, styles}
                 stopTimer(newTime);
             };
             setCurrentTime(newTime);
-            const saveOnNavigate = () => {
-                navigation.addListener('beforeRemove', (e) => {
-                    e.preventDefault();
-                    saveTime(newTime);
-                    navigation.dispatch(e.data.action);
-                });
-            };
-            return saveOnNavigate();
+            return newTime;
         };
-        const interval = setInterval(() => getTime(), 1000);
+        const saveOnNavigate = (timeToSave) => {
+            navigation.addListener('beforeRemove', (e) => {
+                e.preventDefault();
+                saveTime(timeToSave);
+                navigation.dispatch(e.data.action);
+            });
+        };
+        const setUpTime = () => {
+            return saveOnNavigate(getTime());
+        };
+        const interval = setInterval(() => setUpTime(), 1000);
         return () => clearInterval(interval);
     }, [navigation, currentTime, stopTimer, saveTime]);
+
+    // useEffect(() => {
+    //     const getTime = () => {
+    //         const newTime = {...currentTime};
+    //         if (newTime.seconds > 1 || (newTime.minutes >= 1 && newTime.seconds === 1) ) {
+    //             newTime.seconds--;
+    //         } else if (newTime.minutes > 0) {
+    //             newTime.minutes--;
+    //             newTime.seconds = 59;
+    //         } else {
+    //             newTime.minutes = 0;
+    //             newTime.seconds = 0;
+    //             if (newTime.period >= newTime.maxPeriod) newTime.regulation = false;
+    //             else newTime.regulation = true;
+    //             stopTimer(newTime);
+    //         };
+    //         setCurrentTime(newTime);
+    //         const saveOnNavigate = () => {
+    //             navigation.addListener('beforeRemove', (e) => {
+    //                 e.preventDefault();
+    //                 saveTime(newTime);
+    //                 navigation.dispatch(e.data.action);
+    //             });
+    //         };
+    //         return saveOnNavigate();
+    //     };
+    //     const interval = setInterval(() => getTime(), 1000);
+    //     return () => clearInterval(interval);
+    // }, [navigation, currentTime, stopTimer, saveTime]);
 
 
     return (
